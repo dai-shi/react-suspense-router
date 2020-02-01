564008d4c9bd15ff96b4d88662bdd41c8638f756
daishi <daishi@axlight.com>
2020-02-01 22:29:07 +0900

    run compile

--- dist/src/RouteDataContext.d.ts
+++ dist/src/RouteDataContext.d.ts
@@ -2,4 +2,4 @@
 /// <reference types="react/experimental" />
 export declare const RouteDataProvider: import("react").ComponentType<{
     data: object | null;
-}>, useRouteData: () => {} | null, useRouteDataSelector: <V>(selector: (state: {} | null) => V, equalityFn?: import("react-tracked").EqlFn<V> | undefined) => V;
+}>, useRouteData: () => object | null, useRouteDataSelector: <V>(selector: (state: object | null) => V, equalityFn?: import("react-tracked").EqlFn<V> | undefined) => V;
