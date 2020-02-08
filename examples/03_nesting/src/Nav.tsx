99038a031cec44fbcb0306558d79353f73bf22e4
daishi <daishi@axlight.com>
2020-02-08 21:14:56 +0900

    example 03

--- examples/03_nesting/src/Nav.tsx
+++ examples/03_nesting/src/Nav.tsx
@@ -20,6 +20,12 @@ const Nav: React.FC = () => {
         <li>
           <Link to="/user/2">User 2</Link>
         </li>
+        <li>
+          <Link to="/user/3">User 3</Link>
+        </li>
+        <li>
+          <Link to="/user/4">User 4</Link>
+        </li>
       </ul>
     </div>
   );
