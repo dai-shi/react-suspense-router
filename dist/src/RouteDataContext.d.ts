53fb7ee4503b7960eb24b3d753813898d1d053ec
daishi <daishi@axlight.com>
2020-03-07 15:51:47 +0900

    run compile

--- dist/src/RouteDataContext.d.ts
+++ dist/src/RouteDataContext.d.ts
@@ -1,6 +1,7 @@
-/// <reference types="react" />
-export declare const RouteDataProvider: import("react").ComponentType<{
+import React from 'react';
+export declare const RouteDataProvider: React.FC<{
     data: object;
+    setNotify: (notify: () => void) => void;
 }>;
 export declare const useRouteData: () => unknown;
 export declare const useRouteDataSelector: <V>(selector: (state: object) => V, equalityFn?: import("react-tracked").EqlFn<V> | undefined) => V;
