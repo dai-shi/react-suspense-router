bfd19e1f55e735f31a59184812ff3eebb82bd733
daishi <daishi@axlight.com>
2020-06-18 09:00:01 +0900

    run compile

--- dist/src/FetchData.d.ts
+++ dist/src/FetchData.d.ts
@@ -1,9 +1,9 @@
 import { Match } from './types';
 declare type FetchFunc<T extends object> = (m: Match) => Promise<T>;
-export declare const FetchData: <T extends object>(fetchFunc: FetchFunc<T>) => (match: Pick<import("react-router").RouteMatch, "pathname" | "params">) => import("react-suspense-fetch").Suspendable<T, Pick<import("react-router").RouteMatch, "pathname" | "params">>;
+export declare const FetchData: <T extends object>(fetchFunc: FetchFunc<T>) => (match: Match) => import("react-suspense-fetch").Suspendable<T, Pick<import("react-router").RouteMatch, "pathname" | "params">>;
 export declare const FetchDataLazy: <T extends object>(factory: () => Promise<{
     default: FetchFunc<T>;
-}>) => (match: Pick<import("react-router").RouteMatch, "pathname" | "params">) => import("react-suspense-fetch").Suspendable<T, readonly [import("react-suspense-fetch").Suspendable<{
+}>) => (match: Match) => import("react-suspense-fetch").Suspendable<T, readonly [import("react-suspense-fetch").Suspendable<{
     default: FetchFunc<T>;
 }, unknown>, Pick<import("react-router").RouteMatch, "pathname" | "params">]>;
 export {};
